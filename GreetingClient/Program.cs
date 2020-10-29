625adaad1a1426cf8fcd4d29f4111813b8ac2f15
mostafa <mostafa8026@gmail.com>
2020-10-29 13:34:40 +0330

    feat: add csharp grpc server and client.

---
+++ GreetingClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using Helloworld;
+using System;
+
+namespace GreetingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+            String user = "CSharp";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+        }
+    }
+}
---
+++ GreetingServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Core;
+using Helloworld;
+using System;
+using System.Threading.Tasks;
+
+namespace GreetingServer
+{
+    class Program
+    {
+        class GreeterImpl : Greeter.GreeterBase
+        {
+            // Server side handler of the SayHello RPC
+            public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+            {
+                return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+            }
+        }
+
+        const int Port = 50051;
+
+        static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Greeter.BindService(new GreeterImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
